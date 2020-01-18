e187cbeee939913059c27bc1a264ee601cf4ab5c
Matthew Sartain <matthewsartain@me.com>
2020-01-17 18:15:08 -0600

    auth implementation

---
+++ src/pages/App/App.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+import './App.css';
+import { Route, Switch, Redirect } from 'react-router-dom'
+import SignupPage from '../SignupPage/SignupPage'
+import LoginPage from '../LoginPage/LoginPage'
+import userService from '../../utils/userService';
+import NavBar from '../../components/NavBar/NavBar'
+
+
+class App extends Component {
+ 
+  state = {
+    // Initialize user if there's a token, otherwise null
+    user: userService.getUser()
+  
+  }
+  handleLogout = () => {
+    userService.logout();
+    this.setState({user: null});
+  }
+
+  handleSignupOrLogin = () => {
+    this.setState({user: userService.getUser()});
+  }
+
+  render() {
+    return (
+      
+      <div >
+        <NavBar
+          user={this.state.user}
+          handleLogout={this.handleLogout}
+        />
+        <header className='header-footer'>Team Tracker</header>
+        <Switch>
+          {/* <Route exact path="/" render={() =>
+            // <HomePage />
+          }
+          /> */}
+          <Route exact path='/login' render={({ history }) => 
+            <LoginPage
+              history={history}
+              handleSignupOrLogin={this.handleSignupOrLogin}
+            />
+          }/>
+           <Route exact path='/signup' render={({ history }) => 
+            <SignupPage
+              history={history}
+              handleSignupOrLogin={this.handleSignupOrLogin}
+            />
+          }/>
+        </Switch>
+      </div>
+    )
+  }
+}
+export default App;
---
+++ src/utils/userService.js
@@ -0,0 +1,51 @@
+import tokenService from "./tokenService";
+const BASE_URL = "/api/users/";
+
+export default {
+  signup,
+  getUser,
+  logout,
+  login
+};
+
+function login(creds) {
+  return fetch(BASE_URL + "login", {
+    method: "POST",
+    headers: new Headers({ "Content-Type": "application/json" }),
+    body: JSON.stringify(creds)
+  })
+    .then(res => {
+      if (res.ok) return res.json();
+      throw new Error("Bad Credentials!");
+    })
+    .then(({ token }) => tokenService.setToken(token));
+}
+
+function logout() {
+  tokenService.removeToken();
+}
+
+function getUser() {
+  return tokenService.getUserFromToken();
+}
+
+function signup(user) {
+  return (
+    fetch(BASE_URL + "signup", {
+      method: "POST",
+      headers: new Headers({ "Content-Type": "application/json" }),
+      body: JSON.stringify(user)
+    })
+      .then(res => {
+        if (res.ok) return res.json();
+      })
+      // Parameter destructuring!
+      .then(({ token }) => {
+        tokenService.setToken(token);
+      })
+      .catch(error => {
+        console.error(error);
+        throw new Error(error);
+      })
+  );
+}
